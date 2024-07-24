1dde9f1508dca622c2eb8918986efd1311fd2b43
ta10 <tasepuluh123@gmail.com>
2024-07-24 08:33:54 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,276 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'dart:convert';
+import 'dart:ffi';
+
+void main() {
+  runApp(EmbeddedSystemMonitor());
+}
+
+class EmbeddedSystemMonitor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Monitor',
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      showSemanticsDebugger: false,
+      home: MonitorScreen(),
+    );
+  }
+}
+
+class MonitorScreen extends StatefulWidget {
+  @override
+  _MonitorScreenState createState() => _MonitorScreenState();
+}
+
+class _MonitorScreenState extends State<MonitorScreen> {
+  String _timeString = "";
+  String _dateString = "";
+  String _lab_name = "";
+  String _doorstat = "";
+  String _masterstat = "";
+  String _masterName = "";
+  String _section = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _timeString = _formatTime(DateTime.now());
+    _dateString = _formatDate(DateTime.now());
+    _startTimer();
+  }
+
+  void get_data() async {
+    var response_door = await http.get(
+      Uri.parse("http://192.168.2.240:36356/door/status"),
+    );
+    var response_master = await http.get(
+      Uri.parse("http://192.168.2.240:36356/master/status"),
+    );
+    var responseData_door = json.decode(response_door.body);
+    var responseData_master = json.decode(response_master.body);
+    _lab_name = responseData_master['lab'];
+    _masterstat = responseData_master['status'].toUpperCase();
+    _masterName = responseData_master['name'];
+    _section =  responseData_master["section"];
+    if (responseData_door['lock'] == 1) {
+      _doorstat = "LOCKED";
+    } else if (responseData_door['lock'] == 0) {
+      _doorstat = "UNLOCKED";
+    }
+  }
+
+  void _startTimer() {
+    Future.delayed(Duration(seconds: 1), () {
+      if (mounted) {
+        setState(() {
+          _timeString = _formatTime(DateTime.now());
+          _dateString = _formatDate(DateTime.now());
+        });
+        _startTimer();
+        get_data();
+      }
+    });
+  }
+
+  String _formatTime(DateTime dateTime) {
+    return DateFormat('HH : mm : ss').format(dateTime);
+  }
+
+  String _formatDate(DateTime dateTime) {
+    return DateFormat('EEEE, d MMMM yyyy').format(dateTime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Container(
+                    width: 250,
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          _lab_name,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Laboratory",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2),
+                  borderRadius: BorderRadius.circular(5)),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              margin: EdgeInsets.all(10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    _dateString,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(_timeString,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold))
+                ],
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Bagian kiri
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 3),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Section:',
+                            style: TextStyle(
+                                fontSize: 19, fontWeight: FontWeight.bold),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            width: 200,
+                            child: Text(
+                              _section,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight:
+                                    FontWeight.bold, // Batasi jumlah baris
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Bagian tengah
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.person,
+                    size: 150,
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 3),
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      child: Column(
+                        children: [
+                          Text(
+                            _masterName,
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            _masterstat,
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      )),
+                  SizedBox(height: 20),
+                ],
+              ),
+              // Bagian kanan
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 3),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Column(
+                  children: [
+                    Text(
+                      'Door Status: ',
+                      style:
+                          TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      _doorstat,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.arrow_back_ios_sharp,size: 50,),
+              AnimatedTextKit(repeatForever: true, animatedTexts: [
+                TyperAnimatedText("Please Tap Your Card Before Enter the Lab",
+                    speed: Duration(milliseconds: 50),
+                    textStyle: TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold))
+              ])
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
